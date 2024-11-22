428eb445e2db1f9f64cef2841d0ed0d805df109c
natali.r <natasha-razdobreeva@mail.ru>
2024-11-22 03:37:11 +0300

    commitjunit5

--- src/test/java/ru/netology/CashBackHacker/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashBackHacker/CashbackHackServiceTest.java
@@ -1,24 +1,24 @@
 package ru.netology.CashBackHacker;
 
-import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void testRemainWithCashback() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 500;
+        int amount = 900;
         int actual = service.remain(amount);
-        int expected = 500;
-        assertEquals(expected, actual);
+        int expected = 100;
+        assertEquals(actual, expected);
     }
 }
